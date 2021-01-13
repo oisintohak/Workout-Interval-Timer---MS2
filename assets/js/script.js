9c24119082cc88fcac0b65b9957ba4e84869b3d5
Oisin Tohak <oisintohak@gmail.com>
2021-01-13 09:58:12 +0530

    add progress time and total time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@ const timerDisplay = document.querySelector('#t-display');
 const timerStart = document.querySelector('#t-start');
 const timerPause = document.querySelector('#t-pause');
 const timerReset = document.querySelector('#t-reset');
+const timerProgress = document.querySelector('#t-progress');
 const timer = {
   running: false,
   hasStarted: false,
-  workTime: 30,
-  restTime: 15,
+  workTime: 10,
+  restTime: 5,
   numRounds: 9,
   extBreakOn: true,
-  extBreaklength: 30,
+  extBreaklength: 1,
   extBreakAfter: 3,
   timeElapsedOnPause: 0
 };
@@ -17,24 +18,32 @@ const settings = {};
 
 function createRounds(t) {
   t.rounds = {};
-  for (let i = 1; i <= t.numRounds; i ++) {
+  for (let i = 1; i <= t.numRounds*2; i ++) {
     t.rounds[i] = {};
-    t.rounds[i].work = t.workTime;
-    if (i % t.extBreakAfter == 0) {
-      t.rounds[i].rest = t.extBreaklength;
+    if (i % 2 === 0) {
+      if (i % t.extBreakAfter == 0) {
+        t.rounds[i].rest = t.extBreaklength;
+      }
+      else {
+        t.rounds[i].rest = t.restTime;
+      }
+      // calculate runtime for round:
+      t.rounds[i].roundRuntime = t.rounds[i].rest;
     }
     else {
-      t.rounds[i].rest = t.restTime;
+      t.rounds[i].work = t.workTime;
+      // calculate runtime for round:
+      t.rounds[i].roundRuntime = t.rounds[i].work;
+    }
+    // add runtime from any previous rounds:
+    if (i > 1) {
+      t.rounds[i].roundRuntime += t.rounds[i-1].roundRuntime;
     }
   }
 }
 
 function calcRuntime (t) {
-  t.runtime = 0;
-  for (let i = 1;i <= t.numRounds; i++){
-    t.runtime += t.rounds[i].work;
-    t.runtime += t.rounds[i].rest;
-  }
+  t.runtime = t.rounds[t.numRounds * 2].roundRuntime;
 }
 
 function startTimer () {
@@ -52,23 +61,50 @@ function startTimer () {
   timer.startTime = new Date().getTime();
   timer.running = true;
   timer.intervalID = setInterval(countdown, 100);
-  
 };
 
 
 function countdown() {
-  timer.timeElapsed = new Date().getTime() - timer.startTime;
+  timer.timeElapsed = Math.floor((new Date().getTime() - timer.startTime) / 1000);
   // if timer was paused, add previous elapsed time
   timer.timeElapsed += timer.timeElapsedOnPause;
-  console.log(timer.timeElapsed);
-  timer.timeRemaining = timer.runtime - Math.floor((timer.timeElapsed / 1000));
-  displayTime(timer.timeRemaining);
+  timer.timeRemaining = timer.runtime - timer.timeElapsed;
+  checkRound(timer);
+  displayTime();
 }
 
-function displayTime(time) {
-  timerDisplay.textContent = time;
+function displayTime() {
+  timerDisplay.textContent = timer.rounds[timer.currentRound].roundRuntime - timer.timeElapsed;
+  timerProgress.textContent = `${secondsToFullTime(timer.timeElapsed)}/${secondsToFullTime(timer.runtime)}`;
 };
 
+function secondsToFullTime(seconds) {
+  let min = `${Math.floor(seconds / 60)}`;
+  let sec = (seconds % 60).toString();
+  if (sec.length < 2) {
+    sec = `0${sec}`;
+  }
+  return `${min}:${sec}`;
+};
+
+function checkRound(t) {
+  for (let i = 1; i <= t.numRounds; i++) {
+    if (t.timeElapsed >= t.rounds[i].roundRuntime) {
+      t.currentRound = i + 1;
+      if (t.currentRound % 2 === 0) {
+        t.roundType = 'rest';
+      }
+      else {
+        t.roundType = 'work';
+      }
+      //change color scheme
+      //audio alert
+    }
+  }
+  if (t.currentRound === (t.numRounds)) {
+    console.log('finish!');
+  }
+}
 
 function pauseTimer() {
   if (timer.running === true) {
